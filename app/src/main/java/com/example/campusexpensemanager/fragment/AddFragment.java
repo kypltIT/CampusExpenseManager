034918cb5ddc7f05344cadd8b2cad2670d594be3
kypltIT <kyplt.it@gmail.com>
2024-11-06 14:52:53 +0700

    init LoginActivity, RegisterActivity, bottom_nav_menu.xml

---
+++ app/src/main/java/com/example/campusexpensemanager/fragment/AddFragment.java
@@ -0,0 +1,107 @@
+package com.example.campusexpensemanager.fragment;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.campusexpensemanager.R;
+import com.example.campusexpensemanager.model.Category;
+
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    public EditText editTextDate;
+    public Spinner categoriesSpiner;
+    public AddFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddFragment newInstance(String param1, String param2) {
+        AddFragment fragment = new AddFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+        editTextDate = view.findViewById(R.id.editTextDate);
+        editTextDate.setOnClickListener(v -> showDatePickerDialog());
+
+         categoriesSpiner = view.findViewById(R.id.category_id);
+        loadCategoriesIntoSpinner();
+        return view;
+    }
+
+    public void showDatePickerDialog() {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                (view, selectedYear, selectedMonth, selectedDay) -> {
+                    editTextDate.setText(selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear);
+                }, year, month, day);
+        datePickerDialog.show();
+    }
+    public void loadCategoriesIntoSpinner()
+    {
+        Category category = new Category(requireContext());
+        List<Category> formattedCategories = category.getFormattedCategories();
+
+        // Use ArrayAdapter to display category names in the Spinner
+        ArrayAdapter<Category> adapter = new ArrayAdapter<>(this.getContext()
+                , android.R.layout.simple_spinner_item, formattedCategories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoriesSpiner.setAdapter(adapter);
+    }
+
+
+}
